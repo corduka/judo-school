b68f9b667c1f789d43fa153662806114ecec9a19
corduka <cordukabdullah@gmail.com>
2025-10-18 20:47:29 -0500

    Cleaned up the index and applied gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 
 import React from 'react';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 
-// Define your repository name once
-const REPO_NAME = "/judo-school/"; // Must match the base in vite.config.js
-
 const App = () => {
   return (
-    <BrowserRouter basename={REPO_NAME}>
+    <BrowserRouter basename="/judo-school">
       <div className="min-h-screen flex flex-col pt-20"> {/* pt-20 to offset the fixed Navbar height */}
         <Navbar />
         <main className="flex-grow">
